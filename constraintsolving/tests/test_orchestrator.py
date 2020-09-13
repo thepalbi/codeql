088e5f5f8afb6c53eaf46a1284e1e30b472892b6
Pablo Balbi <pablo.l.balbi@gmail.com>
2020-09-13 20:32:41 -0300

    Updating test to regress over generate_model out

--- constraintsolving/tests/test_orchestrator.py
+++ constraintsolving/tests/test_orchestrator.py
@@ -13,27 +13,34 @@ logger = logging.getLogger('test-logger')
 # TODO: Do some automattic test generations based of test_names?
 # TODO: Add test cleanup
 
-def test_generate_entities_regression_with_sql_worse_and_bidding_system():
-    generate_entities_and_assert_regression_passes(
+def test_generate_model_regression_with_sql_worse_and_bidding_system():
+    generate_model_and_assert_regression_passes(
         'output/abhinavkumarl-bidding-system',
         'abhinavkumarl-bidding-system',
         'Sql',
         'SqlInjectionWorse',
-        'test-sql-worse-bidding-system-expected-entities'
+        'test-sql-worse-bidding-system-expected-model'
     )
 
 
-def generate_entities_and_assert_regression_passes(project_dir: str, project_name: str, query_type: str, query_name: str, expected_files_dir: str):
+def generate_model_and_assert_regression_passes(project_dir: str, project_name: str, query_type: str, query_name: str, expected_files_dir: str):
     # Generate actual and expected results paths
     expected_entities_dir = os.path.join(
         global_config.sources_root, 'constraintsolving', 'tests', expected_files_dir)
+    # Actual constraints directory will be the first found
     actual_entities_dir = os.path.join(
-        global_config.sources_root, 'constraintsolving', 'data', project_name)
-    orch = Orchestrator(project_dir, project_name, query_type, query_name)
-    expected_file_count = len(os.listdir(expected_entities_dir))
+        global_config.sources_root, 'constraintsolving', 'constraints', project_name)
+    # Select the first dir on constraints, assuming there's only one
+    constraints_dir_name = os.listdir(actual_entities_dir)[0]
+    actual_entities_dir = os.path.join(
+        actual_entities_dir, constraints_dir_name)
 
-    # Run generate_entities step
+    # Start orchestrator
+    orch = Orchestrator(project_dir, project_name, query_type, query_name)
+
+    # Run generate_entities and generate_model step
     orch.run_step('generate_entities')
+    orch.run_step('generate_model')
 
     # Do results compare
     dir_comparer = filecmp.dircmp(expected_entities_dir, actual_entities_dir)
@@ -41,6 +48,7 @@ def generate_entities_and_assert_regression_passes(project_dir: str, project_nam
     # Assertions
     assert len(
         dir_comparer.diff_files) == 0, f"The following files differ: [{', '.join(dir_comparer.diff_files)}]"
+    expected_file_count = len(os.listdir(expected_entities_dir))
     assert len(
         dir_comparer.same_files) == expected_file_count, f"Expected {expected_file_count} files to be equal, but found just {len(dir_comparer.same_files)}"
     assert len(
