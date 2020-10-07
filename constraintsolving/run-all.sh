9b6e4e9564c7645b093e6030ac4431e8c2ba54d3
Diego Garbervetsky <diego.garbervetsky@gmail.com>
2020-10-07 14:01:51 +0000

    add 0 threshold

--- constraintsolving/run-all.sh
+++ constraintsolving/run-all.sh
@@ -7,17 +7,23 @@ sh ./cleanMetrics.sh
 echo Calling Optimize  on Sql projects
 sh ./run-Sql.sh --single-step optimize $* 
 echo Calling generate_scores on Sql projects
-sh ./run-Sql.sh --single-step generate_scores $* 
+sh ./run-Sql.sh --single-step generate_scores --kind snk $* 
+sh ./run-Sql.sh --single-step generate_scores --kind src $* 
+sh ./run-Sql.sh --single-step generate_scores --kind san $* 
 
 echo Calling Optimize on NoSql projects 
 sh ./run-NoSql.sh --single-step optimize $* 
 echo Calling generate_scores on NoSql projects
-sh ./run-NoSql.sh --single-step generate_scores $* 
+sh ./run-NoSql.sh --single-step generate_scores --kind snk $* 
+sh ./run-NoSql.sh --single-step generate_scores --kind src $* 
+sh ./run-NoSql.sh --single-step generate_scores --kind san $* 
 
 echo Calling Optimize --no-flow on Xss projects
 sh ./run-Xss.sh --single-step optimize $*
 echo Calling generate_scores on Xss projects
-sh ./run-Xss.sh --single-step generate_scores $* 
+sh ./run-Xss.sh --single-step generate_scores --kind snk $* 
+sh ./run-Xss.sh --single-step generate_scores --kind src $* 
+sh ./run-Xss.sh --single-step generate_scores --kind san $* 
 
 
 # now compute the stats 
@@ -35,9 +41,15 @@ python3 misc/combinescores.py --project-dir /persistent/experiments/results/xss/
 sh ./cleanMetrics.sh
 
 # now recompute using combined stats 
-./run-Sql-combined.sh $*
-./run-NoSql-combined.sh  $*
-./run-Xss-combined.sh $*
+./run-Sql-combined.sh --kind snk $*
+./run-Sql-combined.sh --kind src $*
+./run-Sql-combined.sh --kind san $*
+./run-NoSql-combined.sh --kind snk $*
+./run-NoSql-combined.sh --kind src $*
+./run-NoSql-combined.sh --kind san $*
+./run-Xss-combined.sh- -kind snk $*
+./run-Xss-combined.sh- -kind src $*
+./run-Xss-combined.sh- -kind san $*
 
 # now compute generate combined  stats 
 python3 generateMetrics.py --project-list sqlinjection_projects.txt --working-dir /mnt/wrk/sql/ --combined > sql-stats-combined.txt 
